b98e14bf40d7e6ab66226a52e4a57d0b9633afcc
IceLordUlmo <torgall22@gmail.com>
2023-04-21 13:29:24 -0500

    router basics

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,14 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const eventImagesRouter = require('./eventimages.js')
+const eventsRouter = require('./events.js')
+const groupImagesRouter = require('./groupimages.js')
+const groupsRouter = require('./groups.js')
+const venuesRouter = require('./venues.js')
+
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -12,6 +19,16 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/event-images', eventImagesRouter);
+
+router.use('/events', eventsRouter);
+
+router.use('/group-images', groupImagesRouter);
+
+router.use('/groups', groupsRouter);
+
+router.use('/venues', venuesRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
