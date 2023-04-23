f8af110196f226dc4d0ca414d03bb67778085b28
IceLordUlmo <torgall22@gmail.com>
2023-04-23 05:55:11 -0500

    cleaned up unnecessary route from auth me setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,26 +10,13 @@ const apiRouter = require('./api');
 // add API router
 router.use('/api', apiRouter);
 
-// Add a XSRF-TOKEN cookie
+// Add an XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
         'XSRF-Token': csrfToken
     });
-});
-
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../utils/auth.js');
-const { User } = require('../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-    const user = await User.findOne({
-        where: {
-            username: 'Demo-lition'
-        }
-    });
-    setTokenCookie(res, user);
-    return res.json({ user: user });
 });
 
 // export this router
