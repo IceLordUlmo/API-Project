8f411197ee4c9ce620b131e77509f06260c53249
IceLordUlmo <torgall22@gmail.com>
2023-04-23 17:55:21 -0500

    group image deletion

--- backend/routes/api/groupimages.js
+++ backend/routes/api/groupimages.js
@@ -6,11 +6,71 @@ const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { Membership } = require('../../db/models');
 const router = express.Router();
 
 // import express validator and function to validate request bodies
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+
+router.delete("/:imageId", requireAuth, async (req, res) => {
+    const { imageId } = req.params;
+    const userId = req.user.id;
+    const imageIdToDelete = imageId;
+
+    const groupImageToDelete = await GroupImage.findOne({
+        where: {
+            id: imageIdToDelete
+        },
+        include: {
+            model: Group,
+            attributes: ["id", "organizerId"]
+        }
+    })
+
+    if (!groupImageToDelete) {
+        res.status(404);
+        return res.json(
+            {
+                'message': "Group image couldn't be found"
+            }
+        )
+    }
+
+    const groupForDeletedGroupImage = await Group.findOne({
+        where: {
+            id: groupImageToDelete.Event.groupId
+        }
+    })
+
+    const cohostMembershipOfTheUser = await Membership.findOne({
+        where: {
+            groupId: groupImageToDelete.Event.groupId,
+            status: "co-host",
+            userId: userId
+        }
+    })
+
+    let organizerId = groupForDeletedGroupImage.organizerId;
+    // this is feeling very convoluted
+    // if there's no cohost membership and we're not the organizer
+    if (!cohostMembershipOfTheUser &&
+        organizerId !== userId) {
+        let error = { 'message': 'Current User must be the organizer of the group or a member of the group with a status of "co-host"' }
+        res.status(403);
+        return res.json(error)
+    }
+
+    await GroupImage.destroy({
+        where: {
+            id: imageIdToDelete
+        }
+    })
+
+    const objectifyDeletion = { "message": "Successfully deleted" }
+
+    return res.json(objectifyDeletion)
+})
 
 // export it
 module.exports = router;
