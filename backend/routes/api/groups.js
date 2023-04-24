68b3f48019e16a2505093ef6dc88d4a807b99d20
IceLordUlmo <torgall22@gmail.com>
2023-04-24 06:02:37 -0500

    adding EventImage import

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -5,7 +5,7 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Group, Membership, GroupImage, Venue, Event } = require('../../db/models');
+const { User, Group, Membership, GroupImage, Venue, Event, EventImage } = require('../../db/models');
 const router = express.Router();
 const { Op } = require('sequelize');
 // import express validator and function to validate request bodies
