b3cbdfd50cf0f7a9044c6895744c5cb431adaf10
IceLordUlmo <torgall22@gmail.com>
2023-04-24 09:57:08 -0500

    import Group

--- backend/routes/api/groupimages.js
+++ backend/routes/api/groupimages.js
@@ -5,7 +5,7 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Membership, GroupImage } = require('../../db/models');
+const { Membership, GroupImage, Group } = require('../../db/models');
 const router = express.Router();
 
 // import express validator and function to validate request bodies
