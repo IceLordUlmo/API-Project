8c8a81d83d721df6cddf31dcd695b33ae71fb5e5
IceLordUlmo <torgall22@gmail.com>
2023-07-09 23:16:59 -0500

    more styling

--- frontend/src/components/Groups/OneGroupDetails/index.js
+++ frontend/src/components/Groups/OneGroupDetails/index.js
@@ -40,66 +40,88 @@ export const OneGroupDetails = () =>
 
     console.log('in 1', group)
     return (
-        <div>
-            <div className="group-breadcrumb">
-                <div className="group-breadcrumb-spacer">
+        <div className="one-group-external">
+            <div className='one-group-top-half'>
+                <div className="group-breadcrumb">
+                    <div className="group-breadcrumb-spacer">
 
-                </div>
-                <div className="group-breadcrumb-text">
-                    <Link to='/groups'>
-                        Groups
-                    </Link>
-                </div>
-            </div>
-            <div className="group-details">
-
-                <div className="group-detail-text-top">
-                    <h1>{group.name}</h1>
-                    <img src={group.GroupImages[0].url} className="one-group-image-temp">
-
-                    </img>
-                    <p>{group.city}, {group.state}</p>
-                    <div className="event-public-container">
-                        <p>{eventCount} {eventCount === 1 ? 'Event' : 'Events'} • {group.private ? "Private" : "Public"}</p>
                     </div>
-                    <p>
+                    <div className="group-breadcrumb-text">
+                        {'<'}<Link to='/groups'>
+                            Groups
+                        </Link>
+                    </div>
+                </div>
+                <div className="group-details">
+
+                    <div className="group-detail-text-top">
+
+                        <img src={group.GroupImages[0].url} className="one-group-image-temp">
+
+                        </img>
+                        <div>
+                            <h2 className="one-group-name">{group.name}</h2>
+                            <h3 className="one-group-right-text">{group.city}, {group.state}</h3>
+                            <div>
+                                <h3 className="one-group-right-text">{eventCount} {eventCount === 1 ? 'Event' : 'Events'} • {group.private ? "Private" : "Public"}</h3>
+                            </div>
+                            <h3 className="one-group-right-text">
+                                Organized by: {group.Organizer.firstName} {group.Organizer.lastName}
+                            </h3>
+
+                            {(weCreatedThis) &&
+                                (<div className="one-group-horizontal-buttons">
+                                    <Link to={`/groups/${group.id}/events/new`} >
+                                        <button className="one-group-details-button">Create event</button>
+                                    </Link>
+                                    <Link to={`/groups/${group.id}/edit`} >
+                                        <button className="one-group-details-button one-group-details-dg">Update</button>
+                                    </Link>
+
+                                    <div className="one-group-details-button one-group-details-dg">
+                                        <OpenModalMenuItem
+                                            itemText="Delete"
+                                            modalComponent={<DeleteModal className="modal-container-delete" groupId={group.id} />}
+                                        /></div>
+                                </div>
+                                )
+                            }
+                            {(weCanJoinThis) &&
+                                (
+                                    <button className="one-group-details-join-button"
+                                        onClick={joinButton}
+                                    >Join this group</button>
+                                )
+                            }
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+            <div className="one-group-events-wide">
+                <div className="one-group-events">
+                    <h3 className="one-group-bottom-organizer">
                         Organized by: {group.Organizer.firstName} {group.Organizer.lastName}
-                    </p>
+                    </h3>
                     <div>
                         <h3>What we're about:</h3>
                         <p>{group.about}</p>
                     </div>
-                    <div>
+                    {groupEvents.length > 0 ? (<div>
                         <h3>Events ({groupEvents.length})</h3>
                         {groupEvents.map(event => <MiniEvent event={event} key={event.id} />)}
-                    </div>
-
-                    {(weCreatedThis) &&
-                        (<div>
-                            <Link to={`/groups/${group.id}/events/new`} className="one-group-details-button">
-                                Create event
-                            </Link>
-                            <Link to={`/groups/${group.id}/edit`} className="one-group-details-button one-group-details-dg">
-                                Update
-                            </Link>
-
-                            <div className="one-group-details-button one-group-details-dg">
-                                <OpenModalMenuItem
-                                    itemText="Delete"
-                                    modalComponent={<DeleteModal className="modal-container-delete" groupId={group.id} />}
-                                /></div>
-                        </div>
-                        )
-                    }
-                    {(weCanJoinThis) &&
-                        (
-                            <button className="one-group-details-join-button"
-                                onClick={joinButton}
-                            >Join this group</button>
-                        )
+                    </div>) :
+                        (<h3>No Upcoming Events</h3>)
                     }
                 </div>
             </div>
-        </div>
+
+
+
+        </div >
     )
 }
